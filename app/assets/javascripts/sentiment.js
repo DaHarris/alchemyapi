56b48b8b89f4a19167fdc24d95b88a2be36f90c8
Daniel Harris <bobth3bum@gmail.com>
2015-08-06 15:28:26 -0600

    add ajax call to api controller to get sentiment for each tweet and provide color analysis for returned value

---
+++ app/assets/javascripts/sentiment.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  $('#options').on('click', function() {
+    var tweets = $('.tweet');
+    $.each(tweets, function(index, value) {
+      text = $(this).text();
+      $.ajax({
+        url: "/api",
+        data: {text: text}
+      }).success(function(value) {
+        var div = $("div:contains(" + value['text']+ ")").last();
+        var score = parseFloat(value['score']);
+        var hslValues = evaluateScoreForColor(score);
+        div.css('color', hslValues);
+      })
+    });
+  });
+
+  function evaluateScoreForColor(score) {
+    var saturation = "100%";
+    var lightness = "50%";
+    var top = 120;
+
+    score = score + 1;
+    score = score/2;
+    var hue = top*score;
+
+    return "hsl(" + hue + "," + saturation + "," + lightness + ")";
+  }
+
+});
